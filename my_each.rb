0e14ebb202e1f8286806239e42f2dc20117c6920
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-07 16:58:21 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given? == false
+    return array
+  end
+  i=0
+  while i<=array.size-1
+    yield array[i]
+    i+=1
+  end
+  array
 end
